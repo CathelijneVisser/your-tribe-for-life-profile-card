4b2aff2eeff6edd314e7fb6276749d2ab50e0397
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-09-08 16:45:30 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,47 @@
   import { SliceZone } from '@prismicio/svelte'
   import { components } from '$lib/slices'
   export let data
-
 </script>
-
+<SliceZone slices={data.slices} {components} />
 
 <section>
 
 <h1>{data.slices[0].primary.name}</h1>
-<img src="{data.slices[0].primary.picture}">
+<img src="{data.slices[0].primary.picture.url}">
+
 
 </section>
 
+<section>
+    <p>{data.slices[0].primary.age}</p>
+</section>
  
+<section>
+    {data.slices[0].primary.hobbys[0].text}
+</section>
 
- 
+  <!-- <pre>
+{JSON.stringify(data, null, 2)}
+
+  </pre> -->
+
+
+
+<style>
+    body{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    section{
+        border: 2px solid black;
+        width: 10em;
+        height: 10em;
+        padding: 1em;
+        margin: 1em;
+    }
+
+
+</style>
