4712aea5b91e55a9ee8a557dec4a169214775134
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-09-08 15:21:42 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,24 +8,9 @@
 
 
 <section>
-<!-- {#each data.items as item }
-    <p>{item.text}</p>
-{/each} -->
-  <!-- <h1>{data.name[0].text}</h1> -->
-<p>{data.slices[0].primary.name}</p>
 
-  <hr>  
-
-  <SliceZone slices={data.slices} {components} />
-  <pre>
-
-Data uit prismic:
-
- 
-
-{JSON.stringify(data, null, 2)}
-
-  </pre>
+<h1>{data.slices[0].primary.name}</h1>
+<img src="{data.slices[0].primary.picture}">
 
 </section>
 
